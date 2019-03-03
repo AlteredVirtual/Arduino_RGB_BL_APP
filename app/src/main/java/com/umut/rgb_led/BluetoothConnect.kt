abd3c2f8c518a62a7f59bf6133af27d0ee6bb755
Umut Görken <umutgorkenn9@gmail.com>
2019-03-03 17:33:59 +0300

    Show Bluetooth Device List

--- app/src/main/java/com/umut/rgb_led/BluetoothConnect.kt
+++ app/src/main/java/com/umut/rgb_led/BluetoothConnect.kt
@@ -1,5 +1,8 @@
 package com.umut.rgb_led
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,12 +10,28 @@ import android.support.v7.app.AppCompatActivity;
 
 class BluetoothConnect : AppCompatActivity() {
 
+    lateinit var m_bluetoothAdapter: BluetoothAdapter
+    lateinit var m_pairedDevices: Set<BluetoothDevice>
+    val  REQUEST_ENABLE_BLUETOOTH=1
+    companion object {
+        val EXTRA_ADDRESS: String="Device_address"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bluetooth_connect)
 
-        //Show bluetooth devices 
+        m_bluetoothAdapter= BluetoothAdapter.getDefaultAdapter()
+        if(m_bluetoothAdapter==null){
+            toast("this device doesn'tsupport bluethoot")
+            return
+        }
+    }
+    //Show bluetooth devices list
+    private fun pairedDeviceList(){
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
